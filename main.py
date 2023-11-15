e2d0ecfefe05fa9a96f1f61acae0741e052dfb30
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-11-15 21:17:02 +0330

    2023.11.15 - 1 (let's resume in 22,050 Hz...)

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ import matplotlib.pyplot as plot
 
 from config import BYTES_PER_SAMPLE
 
-pcm_path = 'aud/aud.pcm'
+pcm_path = 'aud/1.pcm'
 data: list[int] = []
 with open(pcm_path, 'rb') as pcm:
-    pcm.seek(384)
-    for f in range(384, os.path.getsize(pcm_path), BYTES_PER_SAMPLE):
+    for f in range(0, os.path.getsize(pcm_path), BYTES_PER_SAMPLE):
         data.append(struct.unpack('<h', pcm.read(BYTES_PER_SAMPLE))[0])
-    # the initial noise isn't from the kickstart buffer! it's longer than that
-    # the kickstart buffer is all zeroes, but these are loud numbers!
 plot.plot(data)
 plot.show()
