94b5e785c5b54f4a3dce1f9af3180c9aa3abca79
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-12-01 10:06:39 +0330

    2023.12.01 - 1 (DIVISION INTO WAVES...!)

--- main.py
+++ main.py
@@ -7,8 +7,47 @@ from config import BYTES_PER_SAMPLE
 
 pcm_path = 'aud/1.pcm'
 data: list[int] = []
+decreasing = False
+prv: int = 0
+waves: list[list[int]] = []
+this_wave: list[int] = []
+wavelengths: dict[int, int] = {}
 with open(pcm_path, 'rb') as pcm:
     for f in range(0, os.path.getsize(pcm_path), BYTES_PER_SAMPLE):
-        data.append(struct.unpack('<h', pcm.read(BYTES_PER_SAMPLE))[0])
+        value: int = struct.unpack('<h', pcm.read(BYTES_PER_SAMPLE))[0]
+        if not decreasing and value < prv:
+            decreasing = True
+        elif decreasing and value > prv:
+            waves.append(this_wave)
+            wavelength = len(this_wave)
+            if wavelength not in wavelengths:
+                wavelengths[wavelength] = 1
+            else:
+                wavelengths[wavelength] += 1
+            this_wave = []
+            decreasing = False
+        prv = value
+        data.append(value)
+        this_wave.append(value)
 plot.plot(data)
 plot.show()
+print('Total', len(data), 'samples.')
+print('Total', len(waves), 'waves.')
+print('Wavelengths:', wavelengths)
+# {2: 8247,
+#  3: 19306,
+#  4: 5555,
+#  5: 1592,
+#  6: 358,
+#  7: 79,
+#  8: 19,
+#  9: 7,
+#  10: 2,
+#  11: 1,
+#  1385: 1}
+
+# analyse waves
+for w in waves:
+    if len(w) != 3: continue
+    print(((w[0] + w[2]) / 2) - w[1])
+    # LESSON: the middle wave mostly doesn't illustrate a straight line from the first to the third!
